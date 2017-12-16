8e4fed5375a9495a78ce8b30a77a4e5e59164b02
muskanstha <musk0047@edu.easj.dk>
2017-12-16 15:17:46 +0100

    aa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,68 @@
+import 'zone.js';
+import 'reflect-metadata';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
+
+import { ValidateService } from './services/validate.service';
+import { AuthService } from './services/auth.service';
+import { FlashMessagesModule, FlashMessagesService } from 'angular2-flash-messages';
+
+import { AuthGuard } from './guards/auth.guard';
+import { PostComponent } from './components/post/post.component';
+import { UsersComponent } from './components/users/users.component';
+import { CreatePostComponent } from './components/create-post/create-post.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'register', component: RegisterComponent, pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent, pathMatch: 'full', canActivate: [AuthGuard] },
+  { path: 'profile', component: ProfileComponent, pathMatch: 'full', canActivate: [AuthGuard] },
+  { path: 'posts/:title', component: PostComponent, canActivate: [AuthGuard] },
+  { path: 'post/create', component: CreatePostComponent, canActivate: [AuthGuard] },
+  { path: 'users', component: UsersComponent, pathMatch: 'full', canActivate: [AuthGuard] },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    DashboardComponent,
+    ProfileComponent,
+    PostComponent,
+    UsersComponent,
+    CreatePostComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    ValidateService,
+    FlashMessagesService,
+    AuthService,
+    AuthGuard,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
